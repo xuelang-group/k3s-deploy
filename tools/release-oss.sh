a7e9d9048ff2d402a90d9b527fb8b9bde75e933d
majik <me@yamajik.com>
2020-02-21 12:28:55 +0800

    fix

--- tools/release-oss.sh
+++ tools/release-oss.sh
@@ -6,4 +6,4 @@ VERSION=$(cat version.txt | sed "s/+/-/g")
 
 ossutil cp deployments/ "oss://suanpan-public/k3s/${VERSION}/deployments/" -rf
 ossutil cp tools/deploy.sh "oss://suanpan-public/k3s/deploy.sh" -f
-ossutil cp version.txt "oss://suanpan-public/suanpan-rocket/version.txt" -f
+ossutil cp version.txt "oss://suanpan-public/k3s/version.txt" -f
